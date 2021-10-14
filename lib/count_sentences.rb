095111046c26de4288d06ddfa8064f2e3740614e
Samuel <samlee1097@yahoo.com>
2021-10-13 21:53:32 -0400

    commit tree 527b0c1212c266276a3392ae3048a51709180925

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.chars[self.length-1] == "."
   end
 
   def question?
-
+    self.chars[self.length-1] == "?"
   end
 
   def exclamation?
-
+    self.chars[self.length-1] == "!"
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if{|string| string ==""}.length
   end
 end
